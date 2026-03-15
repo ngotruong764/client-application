9b9093a8a42040df4a68ac64d2e8f9700800fbf2
NamTruong2003 <truongnamvxtd91@gmail.com>
2026-03-15 10:59:25 +0700

    add process bar and time when downloading

--- src/main/java/ClientApplicationMain.java
+++ src/main/java/ClientApplicationMain.java
@@ -3,7 +3,6 @@ import model.FileInfo;
 import model.FileOwner;
 import service.FileService;
 import service.IDirectoryService;
-import service.SocketService;
 
 import java.io.*;
 import java.net.InetAddress;
@@ -12,7 +11,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -21,121 +19,100 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import java.util.zip.GZIPOutputStream;
 
 public class ClientApplicationMain {
+
     private static final Logger logger = Logger.getLogger(ClientApplicationMain.class.getName());
 
-    // Thread pool
-    private static final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+    private static final ThreadPoolExecutor executor =
+            (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 
     private static final String RMI_HOST = "RMI_HOST";
     private static final String RMI_PORT = "RMI_PORT";
     private static final String CLIENT_ID = "CLIENT_ID";
     private static final String FILES_DIRECTORY_PATH = "files";
 
-    // Services
     private static final FileService fileService = new FileService();
 
     public static void main(String[] args) {
-        // Get environment variables
+
         Map<String, String> env = System.getenv();
+
         String rmiHost = env.getOrDefault(RMI_HOST, "localhost");
         int rmiPort = Integer.parseInt(env.getOrDefault(RMI_PORT, "1099"));
         String clientId = env.getOrDefault(CLIENT_ID, UUID.randomUUID().toString());
 
-        // If client ID is not provided -> return
         if (clientId.isEmpty()) {
             logger.info("Client ID is empty");
             return;
         }
+
         logger.info("Client ID: " + clientId);
 
-        SocketService socketService = new SocketService();
-
-        // Establish socket connection
         try (ServerSocket serverSocket = new ServerSocket(0)) {
+
             List<FileInfo> clientFiles = fileService.getAllFiles(FILES_DIRECTORY_PATH);
 
             int assignedPort = serverSocket.getLocalPort();
             String socketAddress = InetAddress.getLocalHost().getHostAddress();
-            logger.info( String.format("Socket established %s:%s", socketAddress, assignedPort));
+
+            logger.info(String.format("Socket established %s:%s", socketAddress, assignedPort));
 
             ClientInfo clientInfo = new ClientInfo();
             clientInfo.setId(clientId);
             clientInfo.setSocketAddr(socketAddress);
             clientInfo.setSocketPort(assignedPort);
 
-            // Getting the RMI registry
             Registry registry = LocateRegistry.getRegistry(rmiHost, rmiPort);
+            IDirectoryService stub =
+                    (IDirectoryService) registry.lookup("IDirectoryService");
 
-            // Looking up the remote object in the registry
-            IDirectoryService stub = (IDirectoryService) registry.lookup("IDirectoryService");
-
-            // Registry files to the central directory server
             stub.registryFile(clientInfo, clientFiles);
 
-            // When a client node starts -> Get all available files with and clientInfo from the server
             List<FileOwner> filesFromServer = stub.getAllAvailableFiles();
 
-            // In the case, no files founded
             if (filesFromServer == null || filesFromServer.isEmpty()) {
                 logger.info("There are no available files!");
             } else {
+
                 filesFromServer.forEach(fileOwner -> {
                     System.out.printf(
                             "File: %s | Owner: %s%n",
                             fileOwner.getFileInfo().getFileName(),
-                            fileOwner.getClientInfos().stream().map(ClientInfo::getId).collect(Collectors.joining(", "))
+                            fileOwner.getClientInfos()
+                                    .stream()
+                                    .map(ClientInfo::getId)
+                                    .collect(Collectors.joining(", "))
                     );
                 });
-
-//                // If we have filesFromServer, but the current client has it -> ignore the files
-//                List<FileOwner> fileOwners = filesFromServer.stream()
-//                        .filter(fileOwner -> fileOwner.getClientInfos().stream().noneMatch(client -> client.getId().equals(clientId)))
-//                        .toList();
-//
-//                if (fileOwners.isEmpty()) {
-//                    logger.info("There are no available files!");
-//                } else {
-//                    fileOwners.forEach(fileOwner -> {
-//                        System.out.printf(
-//                                "File: %s | Owner: %s%n",
-//                                fileOwner.getFileInfo().getFileName(),
-//                                fileOwner.getClientInfos().stream().map(ClientInfo::getId).collect(Collectors.joining(", "))
-//                        );
-//                    });
-//                }
-
             }
 
-
-            // Connect
             while (true) {
 
                 Socket socket = serverSocket.accept();
 
-                new Thread(() -> {
+                executor.execute(() -> {
+
                     try {
 
                         InetSocketAddress remote =
                                 (InetSocketAddress) socket.getRemoteSocketAddress();
 
-                        logger.info("Client IP: " + remote.getAddress().getHostAddress());
+                        logger.info("Client IP: " +
+                                remote.getAddress().getHostAddress());
+
                         logger.info("Client port: " + remote.getPort());
 
                         DataInputStream in =
                                 new DataInputStream(socket.getInputStream());
 
-                        DataOutputStream out =
-                                new DataOutputStream(socket.getOutputStream());
-
-                        // read request
                         String fileName = in.readUTF();
                         long start = in.readLong();
                         long end = in.readLong();
 
-                        logger.info("Download request: " + fileName +
-                                " [" + start + " - " + end + "]");
+                        logger.info("Download request: " +
+                                fileName + " [" + start + " - " + end + "]");
 
                         File file = new File("files/" + fileName);
 
@@ -145,42 +122,51 @@ public class ClientApplicationMain {
                             return;
                         }
 
-                        RandomAccessFile raf = new RandomAccessFile(file, "r");
+                        RandomAccessFile raf =
+                                new RandomAccessFile(file, "r");
 
                         raf.seek(start);
+
+                        GZIPOutputStream gzipOut =
+                                new GZIPOutputStream(socket.getOutputStream());
 
                         byte[] buffer = new byte[4096];
                         long remaining = end - start + 1;
 
                         while (remaining > 0) {
 
-                            int read = raf.read(buffer, 0,
-                                    (int) Math.min(buffer.length, remaining));
+                            int read = raf.read(
+                                    buffer,
+                                    0,
+                                    (int) Math.min(buffer.length, remaining)
+                            );
 
                             if (read == -1) break;
 
-                            out.write(buffer, 0, read);
+                            gzipOut.write(buffer, 0, read);
+
                             remaining -= read;
                         }
 
-                        out.flush();
+                        gzipOut.finish();
+                        gzipOut.flush();
+
                         raf.close();
                         socket.close();
 
-                        logger.info("Chunk sent");
+                        logger.info("Compressed chunk sent");
 
                     } catch (Exception e) {
-                        logger.severe(e.getMessage());
+                        logger.log(Level.SEVERE, e.getMessage());
                     }
-                }).start();
+
+                });
             }
 
         } catch (Exception e) {
+
             logger.log(Level.FINER, e.toString());
             e.printStackTrace();
         }
-
     }
-
-
 }
